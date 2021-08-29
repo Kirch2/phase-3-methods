f29f01ef60e5989ec07e8aaa291eba020597ee68
Chris <Kirch2@gmail.com>
2021-08-29 19:36:40 -0400

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+def halve (num1)
+    if !num1.kind_of? Integer 
+        return nil 
+    end
+    return num1 / 2
+end
